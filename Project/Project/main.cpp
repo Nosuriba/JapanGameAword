b3b6dbb3239d14f9ae63e0fc56afb9cc156e0e0f
nosuriba <norusiba>
2019-03-12 12:23:02 +0900

    mainの更新し忘れ

--- Project/Project/main.cpp
+++ Project/Project/main.cpp
@@ -1,23 +1,10 @@
-#include <DxLib.h>
+#include "Game.h"
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int hShowCmd)
+int main()
 {
-	/// とりあえず、画面の表示をするところまでは修正した（サイズは適当）。
-	DxLib::ChangeWindowMode(TRUE);
-	DxLib::SetGraphMode(640, 640, 32);
-	DxLib::ChangeWindowMode(true);
-	DxLib::SetWindowText("名称未設定だぞ(´･ω･`)");
-	if (DxLib_Init() == -1)
-	{
-		return false;
-	}
-	DxLib::SetDrawScreen(DX_SCREEN_BACK);
-	
-	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
-	{
+	LpGame.Init();
+	LpGame.Run();
+	LpGame.End();
 
-	}
-
-	DxLib_End();
 	return 0;
 }
