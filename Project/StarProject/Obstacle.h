6f720a05060e236bd67115496a73c02b990a4774
taiju1780 <ancient.maou@gmail.com>
2019-05-07 13:58:36 +0900

    img追加

--- Project/StarProject/Obstacle.h
+++ Project/StarProject/Obstacle.h
@@ -8,10 +8,12 @@ class Camera;
 struct ObjectInfo {
 	Vector2 _pos;
 	Size _size;
-	ObjectInfo() :_pos(0, 0), _size(0, 0) {};
-	ObjectInfo(const Vector2 &pos, const Size & size) {
+	Rect _rect;
+	ObjectInfo() :_pos(0, 0), _size(0, 0), _rect(_pos,_size) {};
+	ObjectInfo(const Vector2 &pos, const Size & size, const Rect& rect) {
 		_pos = pos;
 		_size = size;
+		_rect = rect;
 	}
 };
 
@@ -20,6 +22,8 @@ class Obstacle
 private:
 	std::vector<ObjectInfo> _obj;
 	std::shared_ptr<Camera>& _camera;
+
+	int rockimg;
 
 public:
 	Obstacle(std::shared_ptr<Camera>& camera);
