32d78a172825274d18fa6d5afb361fe1ecbf7d1a
1701327 <1701327@PC65211>
2019-05-10 12:29:31 +0900

    ヒトデをキモくしてみた

--- Project/StarProject/Player.h
+++ Project/StarProject/Player.h
@@ -1,20 +1,31 @@
 #pragma once
-#include <array>
 #include <vector>
 #include <memory>
 #include "Processing/Geometry.h"
 
 class Camera;
+class Input;
+
+enum class LEG_STATE {
+	NORMAL,
+	SELECT,
+	SHOT,
+	HOLD
+};
+
+struct Leg {
+	Vector2 tip;		// 足の先端座標
+	Vector2 pos;		// 足の位置
+	Vector2 vel;		// 速度
+	const int T = 10;	// 中間地点の数（分割数）
+	std::vector<Vector2> halfway_point;	// 中間地点
+	LEG_STATE state;	// 足の状態
+};
 
 struct Star {
-	float r;
-	Vector2 center;
-	std::array<Vector2, 5> vertexs;
-	std::array<bool, 5> shot;
-	int axis;
-
-	Star() : r(10), center(0, 0) {}
-	Star(int& inr, Vector2& p) :r(inr), center(p) {}
+	float r;				// 足の長さ
+	Vector2 center;			// 中心
+	std::vector<Leg> legs;	// 足
 };
 
 class Player
@@ -22,9 +33,9 @@ class Player
 private:
 	const std::shared_ptr<Camera>& _camera;
 
-	Star star;
+	Star _star;
 
-	Vector2 vel;
+	Vector2 _vel;
 
 	char Buf[256];
 
@@ -32,7 +43,7 @@ public:
 	Player(const std::shared_ptr<Camera>& c);
 	~Player();
 
-	void Update();
+	void Update(const Input& in);
 	void Draw();
 	Star GetInfo();
 	const std::vector<Vector2> GetShot();
