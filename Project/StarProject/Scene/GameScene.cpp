1274bce1f178585c67e10706124cb81acf598ddf
Nosuriba <tekito>
2019-05-29 15:14:12 +0900

    k

--- Project/StarProject/Scene/GameScene.cpp
+++ Project/StarProject/Scene/GameScene.cpp
@@ -260,7 +260,7 @@ GameScene::GameScene(const int& stagenum)
 
 	_camera.reset(new Camera());
 
-	_pl.reset(new Player(_camera, Vector2(200, 200)));
+	_pl.reset(new Player(_camera));
 
 	_col.reset(new Collision());
 
