dd51f2167eb6fde9268d1d9c2e1e940ac34c7005
HismiN <hisui4183@gmail.com>
2019-05-31 16:05:00 +0900

    なまこ

--- Project/StarProject/Enemy/SeaCucumber.cpp
+++ Project/StarProject/Enemy/SeaCucumber.cpp
@@ -16,8 +16,10 @@ SeaCucumber::SeaCucumber(const std::shared_ptr<Camera>& c, const std::shared_ptr
 	_pL = POS + Vector2(-1, 0) * (SIZE.width / 2.0f);
 	_pR = POS + Vector2(1, 0) * (SIZE.width / 2.0f);
 
-	_particle.emplace_back(std::make_shared<Bubble>(_pL.x, _pL.y, 5000, false, true, 5, 3, 0x660099, c));
-	_particle.emplace_back(std::make_shared<Bubble>(_pR.x, _pR.y, 5000, false, true, 5, 3, 0x660099, c));
+	auto cpos = c->CameraCorrection();
+
+	_particle.emplace_back( std::make_shared<Bubble>(_pL.x - cpos.x, _pL.y - cpos.y, 1000, false, true, 5, 3, 0x660099,c));
+	_particle.emplace_back( std::make_shared<Bubble>(_pR.x - cpos.x, _pR.y - cpos.y, 1000, false, true, 5, 3, 0x660099,c));
 
 	_updater = &SeaCucumber::WaitUpdate;
 }
@@ -88,6 +90,7 @@ void SeaCucumber::MoveUpdate()
 void SeaCucumber::CounterUpdate()
 {
 	auto cpos=_camera->CameraCorrection();
+
 	for (auto& p : _particle)
 	{
 		_particle[0]->SetRota(180);
